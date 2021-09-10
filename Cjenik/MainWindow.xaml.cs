8fb66e5c5293d5ca54364de223dc0b1fc5538a24
Abenny31 <ante_benic@hotmail.com>
2021-09-10 18:56:34 +0200

    first commit

---
+++ Cjenik/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace Cjenik
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void KlijentiBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void UslugeBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void TipUslugeBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+     
+
+        
+    }
+}
